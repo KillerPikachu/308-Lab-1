42c3c1f1f33e3667730e6e45910ec793f800b6bd
KillerPikachu <Yanzhezhang06@gmail.com>
2026-08-31 09:42:15 -0700

    backend

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -39,6 +39,14 @@ const findUserByName = (name) => {
   );
 };
 
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -51,6 +59,7 @@ app.get("/users", (req, res) => {
 });
 */
 
+
 app.get("/users", (req, res) => {
   const name = req.query.name;
   if (name != undefined) {
@@ -61,6 +70,25 @@ app.get("/users", (req, res) => {
     res.send(users);
   }
 });
+
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
+});
+
+
 
 app.listen(port, () => {
   console.log(
